0eb5dc355672ff1be0a025f61a81a78a2fc32a9d
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-07-18 14:05:45 -0400

    1.2.8 and 1.2.9

---
+++ Unit 1/1.2.8 Heating Up/MakingPopcorn.java
@@ -0,0 +1,24 @@
+public class MakingPopcorn
+{
+    public static void main(String[] args)
+    {
+        System.out.println("3 Ways to Heat Anything");
+        System.out.println("Each of the 3 methods below is an example of the three ways that heat can be transferred.");
+        
+        System.out.println("");
+        
+        System.out.println("Conduction");
+        System.out.println("Conduction is heat transfer through matter. Metals conduct heat well, but air does not. This is a direct contact type of heat transfer.");
+        
+        System.out.println("");
+        
+        System.out.println("Convection");
+        System.out.println("Convection is heat transfer by the movement of mass from one place to another. It can take place only in liquids and gases. Heat gained by conduction or radiation is moved by convection.");
+        
+        System.out.println("");
+        
+        System.out.println("Radiation");
+        System.out.println("Radiation can transfer heat through the relative emptiness of space. All other forms of heat transfer require motion of molecules like air or water to move heat.");
+    
+    }
+}
---
+++ Unit 1/1.2.9 Personal Timeline/Timeline.java
@@ -0,0 +1,34 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age     | M/YY - Description");
+            System.out.println("0       | 1/11 1");
+                System.out.println("        |");
+                System.out.println("        |");
+                System.out.println("        |");
+                
+            System.out.println("2       | 1/11 1");
+                System.out.println("        |");
+                System.out.println("        |");
+                System.out.println("        |");
+                
+            System.out.println("4       | 1/11 1");
+                System.out.println("        |");
+                System.out.println("        |");
+                System.out.println("        |");
+                
+            System.out.println("6       | 1/11 1");
+                System.out.println("        |");
+                System.out.println("        |");
+                System.out.println("        |");
+                
+            System.out.println("8       | 1/11 1");
+                System.out.println("        |");
+                System.out.println("        |");
+                System.out.println("        |");
+                
+            System.out.println("10      | 1/11 1");
+                
+    }
+}
