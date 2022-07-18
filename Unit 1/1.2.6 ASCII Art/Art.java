98c8d754546da3361c3c8d66d4b8b4bf58563562
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-07-18 13:56:51 -0400

    1.2.6

---
+++ Unit 1/1.2.6 ASCII Art/Art.java
@@ -0,0 +1,11 @@
+public class Art 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("    /)___");
+        System.out.println("   /     O|___");
+        System.out.println("  /           )");
+        System.out.println(" /       )___/");
+    }
+
+}
