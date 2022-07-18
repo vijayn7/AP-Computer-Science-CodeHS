5d8a0c5f541d31a70f8300df17da89c85cd22d68
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-07-18 14:15:00 -0400

    1.3.8 and 1.3.9

---
+++ Unit 1/1.3.8 Answering Questions/Variables.java
@@ -0,0 +1,15 @@
+public class Variables 
+{
+    public static void main(String[] args)
+    {
+        String myName = "Karel the Dog";
+        int luckyNumber = 11;
+        double currentTemperature = 75.3;
+        boolean isStudent = true;
+        
+        System.out.println(myName);
+        System.out.println(luckyNumber);
+        System.out.println(currentTemperature);
+        System.out.println(isStudent);
+    }
+}
