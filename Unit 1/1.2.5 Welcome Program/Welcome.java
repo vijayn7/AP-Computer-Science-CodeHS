7bc5f7ab75fcd7897cc1ca5791d511fa1319df63
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-07-18 13:54:33 -0400

    1.2.5

---
+++ Unit 1/1.2.5 Welcome Program/Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Vijay N.");
+        System.out.println("I like cats");
+        // Your code will go here
+        // These lines are comments
+        // Java ignores anything that comes after 
+        // two forward slashes
+        // You can delete the comments or leave them in
+        // Java ignores them anyway!
+    } 
+}
