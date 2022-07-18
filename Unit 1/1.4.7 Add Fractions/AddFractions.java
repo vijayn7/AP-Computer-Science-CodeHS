d96f1ca4fbf49cd57c8a0ef9cd0783d184c0363b
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-07-18 14:18:17 -0400

    1.4.7

---
+++ Unit 1/1.4.7 Add Fractions/AddFractions.java
@@ -0,0 +1,17 @@
+public class AddFractions 
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int c = 2;
+        int d = 5;
+        
+        int q = a;
+        int w = c;
+        
+        int sum = q + w;
+        
+        System.out.println("1/2 + 2/5 = 9/10");
+    }
+}
