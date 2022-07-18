a7eef2adbacd8985e2ae498aa87b2f2e83de6b52
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-07-18 14:00:07 -0400

    1.2.7

---
+++ Unit 1/1.2.7 Fixing a Paragraph/TomatoEssay.java
@@ -0,0 +1,11 @@
+public class TomatoEssay
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        System.out.println("Is a tomato a fruit or a vegetable?"); //l
+        System.out.println("While biologically speaking a tomato is a fruit, legally speaking it is a vegetable!");
+        System.out.println("In Supreme Court Case Nix v. Hedden, the Court ruled that tomatoes should be classified as a vegetable under U.S Customs regulations because they are consumed more like a vegetable than a fruit, and should be taxed as such.");
+        System.out.println("When tomatoes are shipped into the U.S, they are now taxed as vegetables even though their anatomy suggests otherwise.");
+    }
+}
