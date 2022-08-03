2c459232066d7b96cb95be02b18558731c902af4
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2022-08-02 21:58:01 -0400

    ArrayLists - Level 2

---
+++ Practice problem solutions/ArrayLists - Level 1/ArrayListLastElement/Scratchpad.java
@@ -0,0 +1,18 @@
+/* This is the Scratchpad!
+This file is not graded, but you can use it to test your code.
+
+You can write and test your method in the Scratchpad, but make
+sure to copy and paste it into the Unit Test file before checking
+your answer. Remember to only copy and paste the method you want
+to submit, not all of your tests.
+*/
+
+public class Scratchpad extends ConsoleProgram
+{
+    public void run()
+    {
+        // Add your own tests here
+    }
+
+    // Copy and paste your Unit Test method here
+}
